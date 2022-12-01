aca29e8a4dee16659e25b51d23f5a997dea4eee3
Shane Mc Cormack <shanemcc@gmail.com>
2022-12-01 05:06:11 +0000

    Dedupe.

--- 1/run.php
+++ 1/run.php
@@ -3,19 +3,14 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLineGroups();
 
-	$part1 = 0;
-	foreach ($input as $id => $elf) {
-		$count = array_sum($elf);
-		if ($count > $part1) { $part1 = $count; }
-	}
-
-	echo 'Part 1: ', $part1, "\n";
-
 	$all = [];
 	foreach ($input as $id => $elf) {
 		$all[] = array_sum($elf);
 	}
 	rsort($all);
+
+	$part1 = $all[0];
+	echo 'Part 1: ', $part1, "\n";
 
 	$part2 = $all[0] + $all[1] + $all[2];
 	echo 'Part 2: ', $part2, "\n";
