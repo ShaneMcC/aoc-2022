373a38036a27b756b80af2a0b376f84039c328c0
Shane Mc Cormack <shanemcc@gmail.com>
2022-12-02 05:09:32 +0000

    Day 2. Part 2. 597

--- 2/run.php
+++ 2/run.php
@@ -6,37 +6,53 @@
 	$theirs = ['A' => 'Rock', 'B' => 'Paper', 'C' => 'Scissors'];
 	$ours = ['X' => 'Rock', 'Y' => 'Paper', 'Z' => 'Scissors'];
 
-	$entries = [];
-	foreach ($input as $line) {
-		preg_match('#(.*) (.*)#SADi', $line, $m);
-		[$all, $them, $us] = $m;
-		$entries[] = [$theirs[$them], $ours[$us]];
-	}
-
-
 	$shapeValue = ['Rock' => 1, 'Paper' => 2, 'Scissors' => 3];
 	$resultValue = ['Loss' => 0, 'Draw' => 3, 'Win' => 6];
 	$winning = ['Rock' => 'Scissors', 'Paper' => 'Rock', 'Scissors' => 'Paper'];
+	$losing = ['Scissors' => 'Rock', 'Rock' => 'Paper', 'Paper' => 'Scissors'];
 
+	$entries1 = [];
+	$entries2 = [];
+	foreach ($input as $line) {
+		preg_match('#(.*) (.*)#SADi', $line, $m);
+		[$all, $them, $us] = $m;
 
-	$score = 0;
-	foreach ($entries as $game) {
-		[$them, $us] = $game;
+		$entries1[] = [$theirs[$them], $ours[$us]];
 
-		if ($them == $us) {
-			$score += $resultValue['Draw'];
-		} else if ($winning[$us] == $them) {
-			$score += $resultValue['Win'];
-		} else {
-			$score += $resultValue['Loss'];
+		if ($us == 'X') {
+			$us = $winning[$theirs[$them]];
+		} else if ($us == 'Y') {
+			$us = $theirs[$them];
+		} else if ($us == 'Z') {
+			$us = $losing[$theirs[$them]];
+		}
+		$entries2[] = [$theirs[$them], $us];
+	}
+
+	function getScore($entries) {
+		global $shapeValue, $resultValue, $winning;
+
+		$score = 0;
+		foreach ($entries as $game) {
+			[$them, $us] = $game;
+
+			if ($them == $us) {
+				$score += $resultValue['Draw'];
+			} else if ($winning[$us] == $them) {
+				$score += $resultValue['Win'];
+			} else {
+				$score += $resultValue['Loss'];
+			}
+
+			$score += $shapeValue[$us];
 		}
 
-		$score += $shapeValue[$us];
+		return $score;
 	}
 
 
-	$part1 = $score;
+	$part1 = getScore($entries1);
 	echo 'Part 1: ', $part1, "\n";
 
-	// $part2 = -1;
-	// echo 'Part 2: ', $part2, "\n";
+	$part2 = getScore($entries2);
+	echo 'Part 2: ', $part2, "\n";
