6e282a2abeefa2ddb607629da142a5385d6c7475
Shane Mc Cormack <shanemcc@gmail.com>
2022-12-03 05:14:20 +0000

    Day 3. Part 2. 1971.

--- 3/run.php
+++ 3/run.php
@@ -4,10 +4,19 @@
 	$input = getInputLines();
 
 	$rucksacks = [];
+	$groups = [];
+	$group = [];
 	foreach ($input as $line) {
 		$split = str_split($line, strlen($line)/2);
-		$rucksacks[] = [str_split($split[0]), str_split($split[1])];
+		$r = [str_split($split[0]), str_split($split[1]), str_split($line)];
+		$rucksacks[] = $r;
+		$group[] = $r;
+		if (count($group) == 3) {
+			$groups[] = $group;
+			$group = [];
+		}
 	}
+	if (!empty($group)) { $groups[] = $group; }
 
 	$part1 = 0;
 	foreach ($rucksacks as $r) {
@@ -26,8 +35,24 @@
 			}
 		}
 	}
-
 	echo 'Part 1: ', $part1, "\n";
 
-	// $part2 = -1;
-	// echo 'Part 2: ', $part2, "\n";
+	$part2 = 0;
+	foreach ($groups as $g) {
+		[$first, $second, $third] = $g;
+
+		foreach ($first[2] as $c) {
+			if (in_array($c, $second[2]) && in_array($c, $third[2])) {
+				if (strtolower($c) == $c) {
+					$p = ord($c) - 96;
+				} else {
+					$p = ord($c) - 38;
+				}
+
+				$part2 += $p;
+				break;
+			}
+		}
+	}
+
+	echo 'Part 2: ', $part2, "\n";
