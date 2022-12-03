4e9a8cd6ae2fe2f08e1808e686082751c4d7ee0a
Shane Mc Cormack <shanemcc@gmail.com>
2022-12-03 05:23:08 +0000

    Loop through the rucksacks only once for both parts.

--- 3/run.php
+++ 3/run.php
@@ -4,55 +4,36 @@
 	$input = getInputLines();
 
 	$rucksacks = [];
-	$groups = [];
-	$group = [];
 	foreach ($input as $line) {
 		$split = str_split($line, strlen($line)/2);
 		$r = [str_split($split[0]), str_split($split[1]), str_split($line)];
 		$rucksacks[] = $r;
-		$group[] = $r;
-		if (count($group) == 3) {
-			$groups[] = $group;
-			$group = [];
-		}
 	}
-	if (!empty($group)) { $groups[] = $group; }
 
-	$part1 = 0;
-	foreach ($rucksacks as $r) {
-		[$first, $second] = $r;
+	function getPriority($c) {
+		return ord($c) - (strtolower($c) == $c ? 96 : 38);
+	}
+
+
+	$part1 = $part2 = 0;
+	for ($i = 0; $i < count($rucksacks); $i++) {
+		[$first, $second] = $rucksacks[$i];
 
 		foreach ($first as $c) {
 			if (in_array($c, $second)) {
-				if (strtolower($c) == $c) {
-					$p = ord($c) - 96;
-				} else {
-					$p = ord($c) - 38;
-				}
-
-				$part1 += $p;
+				$part1 += getPriority($c);
 				break;
+			}
+		}
+
+		if ($i % 3 == 0) {
+			foreach ($rucksacks[$i][2] as $c) {
+				if (in_array($c, $rucksacks[$i + 1][2]) && in_array($c, $rucksacks[$i + 2][2])) {
+					$part2 += getPriority($c);
+					break;
+				}
 			}
 		}
 	}
 	echo 'Part 1: ', $part1, "\n";
-
-	$part2 = 0;
-	foreach ($groups as $g) {
-		[$first, $second, $third] = $g;
-
-		foreach ($first[2] as $c) {
-			if (in_array($c, $second[2]) && in_array($c, $third[2])) {
-				if (strtolower($c) == $c) {
-					$p = ord($c) - 96;
-				} else {
-					$p = ord($c) - 38;
-				}
-
-				$part2 += $p;
-				break;
-			}
-		}
-	}
-
 	echo 'Part 2: ', $part2, "\n";
