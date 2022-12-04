27890188d6b3ab449cb6a4ef0be7392dffa7eaae
Shane Mc Cormack <shanemcc@gmail.com>
2022-12-04 07:09:31 +0000

    Day 4. Part 2. 20350.

--- 4/run.php
+++ 4/run.php
@@ -3,8 +3,7 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLines();
 
-	$part1 = 0;
-	$entries = [];
+	$part1 = $part2 = 0;
 	foreach ($input as $line) {
 		preg_match('#(.*)-(.*),(.*)-(.*)#SADi', $line, $m);
 		[, $s1, $e1, $s2, $e2] = $m;
@@ -14,9 +13,13 @@
 
 		if ($contains1 || $contains2) { $part1++; }
 
+		$overlap1 = ($s2 <= $e1 && $s2 >= $s1);
+		$overlap2 = ($s1 <= $e2 && $s1 >= $s2);
+
+		if ($overlap1 || $overlap2) {
+			$part2++;
+		}
 	}
 
 	echo 'Part 1: ', $part1, "\n";
-
-	// $part2 = -1;
-	// echo 'Part 2: ', $part2, "\n";
+	echo 'Part 2: ', $part2, "\n";
