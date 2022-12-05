0cb659aca0789f19a2eb6e675c3d41975c97db86
Shane Mc Cormack <shanemcc@gmail.com>
2022-12-05 05:16:33 +0000

    Day 5. Part 2. 1121.

--- 5/run.php
+++ 5/run.php
@@ -18,17 +18,32 @@
 		}
 	}
 
-	foreach ($moves as $line) {
-		preg_match('#move (.*) from (.*) to (.*)#SADi', $line, $m);
-		[, $count, $from, $to] = $m;
+	function moveCrates($stacks, $moves, $part1 = true) {
+		foreach ($moves as $line) {
+			preg_match('#move (.*) from (.*) to (.*)#SADi', $line, $m);
+			[, $count, $from, $to] = $m;
 
-		$removed = array_reverse(array_splice($stacks[$from], 0, $count));
-		array_unshift($stacks[$to], ...$removed);
+			$removed = array_splice($stacks[$from], 0, $count);
+			if ($part1) { $removed = array_reverse($removed); }
+			array_unshift($stacks[$to], ...$removed);
+		}
+
+		return $stacks;
 	}
 
+	$stacks1 = moveCrates($stacks, $moves);
 	$part1 = '';
-	for ($i = 1; $i <= count($stacks); $i++) {
-		$part1 .= $stacks[$i][0];
+	for ($i = 1; $i <= count($stacks1); $i++) {
+		$part1 .= $stacks1[$i][0];
 	}
 
 	echo 'Part 1: ', $part1, "\n";
+
+
+	$stacks2 = moveCrates($stacks, $moves, false);
+	$part2 = '';
+	for ($i = 1; $i <= count($stacks2); $i++) {
+		$part2 .= $stacks2[$i][0];
+	}
+
+	echo 'Part 2: ', $part2, "\n";
