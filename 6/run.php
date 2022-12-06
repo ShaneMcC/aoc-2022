9369b333ddfd5228e2c957ff141e5eb3fa101f4c
Shane Mc Cormack <shanemcc@gmail.com>
2022-12-06 05:08:57 +0000

    Simplify.

--- 6/run.php
+++ 6/run.php
@@ -3,22 +3,14 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLine();
 
-	for ($i = 3; $i < strlen($input); $i++) {
-		$last4 = str_split(substr($input, $i - 3, 4));
-		if (count(array_unique($last4)) == 4) {
-			$part1 = $i;
-			break;
+	function getStartMarker($input, $length) {
+		for ($i = $length - 1; $i < strlen($input); $i++) {
+			$last = str_split(substr($input, $i - $length - 1, $length));
+			if (count(array_unique($last)) == $length) {
+				return $i + 1;
+			}
 		}
 	}
 
-	echo 'Part 1: ', $part1 + 1, "\n";
-
-	for ($i = 13; $i < strlen($input); $i++) {
-		$last = str_split(substr($input, $i - 13, 14));
-		if (count(array_unique($last)) == 14) {
-			$part2 = $i;
-			break;
-		}
-	}
-
-	echo 'Part 2: ', $part2 + 1, "\n";
+	echo 'Part 1: ', getStartMarker($input, 4), "\n";
+	echo 'Part 2: ', getStartMarker($input, 14), "\n";
