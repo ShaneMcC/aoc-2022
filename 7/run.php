a6cf307fe4d231db40da64be3dcfae758143f3d3
Shane Mc Cormack <shanemcc@gmail.com>
2022-12-07 06:00:37 +0000

    We can actually parse this as we go through the file.

--- 7/run.php
+++ 7/run.php
@@ -3,31 +3,11 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLines();
 
-	// Get commands and their output from the input
-	$commands = [];
-	$command = '';
-	$commandOutput = [];
-	foreach ($input as $line) {
-		if (preg_match('#\$ (.*)#SADi', $line, $m)) {
-			[, $cmd] = $m;
-			if (!empty($command)) { $commands[] = ['cmd' => $command, 'output' => $commandOutput]; }
-			$command = $cmd;
-			$commandOutput = [];
-		} else {
-			$commandOutput[] = $line;
-		}
-	}
-	if (!empty($command)) { $commands[] = ['cmd' => $command, 'output' => $commandOutput]; }
-
-	// Parse the output of the commands into a tree.
 	$tree = [];
 	$pwd = '/';
-	foreach ($commands as $c) {
-		$cmd = $c['cmd'];
-		$out = $c['output'];
+	foreach ($input as $line) {
 
-		// Track current directory.
-		if (preg_match('#cd (.*)#SADi', $cmd, $m)) {
+		if (preg_match('#\$ cd (.*)#SADi', $line, $m)) {
 			if ($m[1] == '..') {
 				$pwd = (dirname($pwd) == '/') ? '/' : dirname($pwd) . '/';
 			} else if ($m[1][0] == '/') {
@@ -35,18 +15,15 @@
 			} else {
 				$pwd .= $m[1] . '/';
 			}
-		// Track files in directory
-		} else if ($cmd == 'ls') {
-			foreach ($out as $lsout) {
-				if (preg_match('#(\d+)\s+(.*)$#SADi', $lsout, $m)) {
-					$file = $pwd . $m[2];
-					$size = $m[1];
+		} else if (!preg_match('#\$ ls#SADi', $line, $m)) {
+			if (preg_match('#(\d+)\s+(.*)$#SADi', $line, $m)) {
+				$file = $pwd . $m[2];
+				$size = $m[1];
 
-					while ($file != '/') {
-						$file = (dirname($file) == '/') ? '/' : dirname($file) . '/';
-						if (!isset($tree[$file])) { $tree[$file] = 0; }
-						$tree[$file] += $size;
-					}
+				while ($file != '/') {
+					$file = (dirname($file) == '/') ? '/' : dirname($file) . '/';
+					if (!isset($tree[$file])) { $tree[$file] = 0; }
+					$tree[$file] += $size;
 				}
 			}
 		}
