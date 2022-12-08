3f8c352459c382f066ea5b7762e433fa23e92a56
Shane Mc Cormack <shanemcc@gmail.com>
2022-12-08 05:21:18 +0000

    Day 8. Part 2. 955

--- 8/run.php
+++ 8/run.php
@@ -3,7 +3,7 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$map = getInputMap();
 
-	$part1 = 0;
+	$part1 = $part2 = 0;
 	foreach ($map as $y => $row) {
 		foreach ($row as $x => $tree) {
 			$before = array_slice($row, 0, $x);
@@ -18,13 +18,20 @@
 			        || empty(array_filter($above, fn($a) => $a >= $tree))
 			        || empty(array_filter($below, fn($a) => $a >= $tree));
 
-			if ($visible) {
-				$part1++;
-			}
+			if ($visible) { $part1++; }
+
+			$before = array_reverse($before);
+			$above = array_reverse($above);
+
+			$before = array_reduce($before, fn($c, $i) => $c[1] ? [$c[0] + 1, $i < $tree] : $c, [0, true])[0];
+			$after = array_reduce($after, fn($c, $i) => $c[1] ? [$c[0] + 1, $i < $tree] : $c, [0, true])[0];
+			$above = array_reduce($above, fn($c, $i) => $c[1] ? [$c[0] + 1, $i < $tree] : $c, [0, true])[0];
+			$below = array_reduce($below, fn($c, $i) => $c[1] ? [$c[0] + 1, $i < $tree] : $c, [0, true])[0];
+
+			$scenic = $before * $after * $above * $below;
+			$part2 = max($part2, $scenic);
 		}
 	}
 
 	echo 'Part 1: ', $part1, "\n";
-
-	// $part2 = -1;
-	// echo 'Part 2: ', $part2, "\n";
+	echo 'Part 2: ', $part2, "\n";
