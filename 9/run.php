b9ce49728e0568f4409bc13b2de7496657825ee9
Shane Mc Cormack <shanemcc@gmail.com>
2022-12-09 05:42:58 +0000

    Day 9. Part 2. 2553.

--- 9/run.php
+++ 9/run.php
@@ -6,13 +6,44 @@
 	$tail = [0, 0];
 	$head = [0, 0];
 
+	$rope = array_fill(0, 10, [0, 0]);
+
 	$directions = ['U' => [0, 1],
 	               'D' => [0, -1],
 	               'R' => [1, 0],
 	               'L' => [-1, 0],
 	              ];
 
-	$tailPositions = [];
+	function moveKnot($head, $tail) {
+		$yDiff = abs($tail[1] - $head[1]);
+		$xDiff = abs($tail[0] - $head[0]);
+
+		// Move the tail to account for the head.
+		if ($yDiff == 2) {
+			if ($tail[1] > $head[1]) { $tail[1]--; }
+			if ($tail[1] < $head[1]) { $tail[1]++; }
+
+			if ($xDiff == 1) {
+				if ($tail[0] > $head[0]) { $tail[0]--; }
+				if ($tail[0] < $head[0]) { $tail[0]++; }
+			}
+		}
+
+		if ($xDiff == 2) {
+			if ($tail[0] > $head[0]) { $tail[0]--; }
+			if ($tail[0] < $head[0]) { $tail[0]++; }
+
+			if ($yDiff == 1) {
+				if ($tail[1] > $head[1]) { $tail[1]--; }
+				if ($tail[1] < $head[1]) { $tail[1]++; }
+			}
+		}
+
+		return $tail;
+	}
+
+	$tailPositions = [1 => [], 9 => []];
+
 	foreach ($input as $in) {
 		[$d, $c] = explode(' ', $in, 2);
 
@@ -20,38 +51,22 @@
 
 		for ($i = 0; $i < $c; $i++) {
 			// Move the head
-			$head[0] += $directions[$d][0];
-			$head[1] += $directions[$d][1];
+			$rope[0][0] += $directions[$d][0];
+			$rope[0][1] += $directions[$d][1];
 
-			$yDiff = abs($tail[1] - $head[1]);
-			$xDiff = abs($tail[0] - $head[0]);
-
-			// Move the tail to account for the head.
-			if ($yDiff == 2) {
-				if ($tail[1] > $head[1]) { $tail[1]--; }
-				if ($tail[1] < $head[1]) { $tail[1]++; }
-
-				if ($xDiff == 1) {
-					if ($tail[0] > $head[0]) { $tail[0]--; }
-					if ($tail[0] < $head[0]) { $tail[0]++; }
-				}
+			for ($k = 1; $k < count($rope); $k++) {
+				$rope[$k] = moveKnot($rope[$k - 1], $rope[$k]);
 			}
 
-			if ($xDiff == 2) {
-				if ($tail[0] > $head[0]) { $tail[0]--; }
-				if ($tail[0] < $head[0]) { $tail[0]++; }
+			if (isDebug()) { echo "\t", json_encode($rope), "\n"; }
 
-				if ($yDiff == 1) {
-					if ($tail[1] > $head[1]) { $tail[1]--; }
-					if ($tail[1] < $head[1]) { $tail[1]++; }
-				}
-			}
-
-			if (isDebug()) { echo "\t", json_encode(['head' => $head, 'tail' => $tail]), "\n"; }
-
-			$tailPositions[json_encode($tail)] = true;
+			$tailPositions[1][json_encode($rope[1])] = true;
+			$tailPositions[9][json_encode($rope[9])] = true;
 		}
 	}
 
-	$part1 = count($tailPositions);
+	$part1 = count($tailPositions[1]);
 	echo 'Part 1: ', $part1, "\n";
+
+	$part2 = count($tailPositions[9]);
+	echo 'Part 2: ', $part2, "\n";
