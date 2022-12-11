664b49b7dd23cf515b25f6f271815ed91172d87f
Shane Mc Cormack <shanemcc@gmail.com>
2022-12-11 06:25:09 +0000

    Reduce bits of code.

--- 11/run.php
+++ 11/run.php
@@ -13,26 +13,14 @@
 			} else if (preg_match('#Starting items: (.*)$#', $line, $m)) {
 				$monkey['items'] = explode(',', str_replace(' ', '', $m[1]));
 			} else if (preg_match('#Operation: new = old (.*) (.*)$#', $line, $m)) {
-				switch ($m[1]) {
-					case '*':
-						$monkey['operation'] = fn($x) => $x * ($m[2] == 'old' ? $x : $m[2]);
-						break;
-					case '-':
-						$monkey['operation'] = fn($x) => $x - ($m[2] == 'old' ? $x : $m[2]);
-						break;
-					case '+':
-						$monkey['operation'] = fn($x) => $x + ($m[2] == 'old' ? $x : $m[2]);
-						break;
-					default:
-						die('Unknown operation: ' . $line);
-				}
+				if ($m[1] == '*') { $monkey['operation'] = fn($x) => $x * ($m[2] == 'old' ? $x : $m[2]); }
+				else if ($m[1] == '+') { $monkey['operation'] = fn($x) => $x + ($m[2] == 'old' ? $x : $m[2]); }
+				else if ($m[1] == '-') { $monkey['operation'] = fn($x) => $x - ($m[2] == 'old' ? $x : $m[2]); }
+				else { die('Unknown operation: ' . $line); }
 			} else if (preg_match('#Test: divisible by (.*)$#', $line, $m)) {
-				$monkey['test'] = [];
-				$monkey['test']['condition'] = $m[1];
-			} else if (preg_match('#If true: throw to monkey (.*)$#', $line, $m)) {
-				$monkey['test'][true] = $m[1];
-			} else if (preg_match('#If false: throw to monkey (.*)$#', $line, $m)) {
-				$monkey['test'][false] = $m[1];
+				$monkey['test'] = ['condition' => $m[1]];
+			} else if (preg_match('#If (true|false): throw to monkey (.*)$#', $line, $m)) {
+				$monkey['test'][$m[1] == 'true'] = $m[2];
 			} else {
 				die('Unknown line: ' . $line);
 			}
@@ -42,42 +30,30 @@
 	}
 
 
-	function processRound(&$monkies, $part2 = false, $debugging = null) {
-		$allDivisors = 1;
-		foreach ($monkies as $m) {
-			$allDivisors *= $m['test']['condition'];
-		}
-
+	function processRound(&$monkies, $worryReduction = null, $debugging = null) {
 		$debugging ??= isDebug();
 
-		foreach (array_keys($monkies) as $mid) {
-			$monkey = &$monkies[$mid];
-
-			if ($debugging) { echo 'Monkey ', $mid, ':', "\n"; }
+		foreach ($monkies as &$monkey) {
+			if ($debugging) { echo 'Monkey ', $monkey['id'], ':', "\n"; }
 
 			foreach ($monkey['items'] as $item) {
+				$monkey['inspectCount']++;
 				if ($debugging) { echo "\t", 'Monkey inspects an item with a worry level of ', $item, "\n"; }
+
 				$item = $monkey['operation']($item);
 				if ($debugging) { echo "\t\t", 'Worry level is now: ', $item, "\n"; }
 
-				if ($part2) {
-					$item = $item % $allDivisors;
-					if ($debugging) { echo "\t\t", 'Worry level is managed by mod ', $allDivisors, ' to ', $item, "\n"; }
-				} else {
-					$item = floor($item / 3);
-					if ($debugging) { echo "\t\t", 'Monkey gets bored with item. Worry level is divided by 3 to ', $item, "\n"; }
-				}
+				$item = $worryReduction($item);
+				if ($debugging) { echo "\t\t", 'Worry level reduction, item is now ', $item, "\n"; }
 
 				$result = ($item % $monkey['test']['condition'] == 0);
 				$newMonkey = $monkey['test'][$result];
+				$monkies[$newMonkey]['items'][] = $item;
 
 				if ($debugging) {
 					echo "\t\t", 'Current worry level ', ($result ? 'is' : 'is not'), ' divisible by: ', $monkey['test']['condition'], "\n";
 					echo "\t\t", 'Item with worry level ', $item, ' is thrown to ', $newMonkey, "\n";
 				}
-
-				$monkies[$newMonkey]['items'][] = $item;
-				$monkey['inspectCount']++;
 			}
 			$monkey['items'] = [];
 		}
@@ -85,13 +61,10 @@
 
 	$startingMonkies = $monkies;
 
+	$worryReduction = fn($item) => floor($item / 3);
 	for ($i = 0; $i < 20; $i++) {
-		if (isDebug()) {
-			echo '==========', "\n";
-			echo 'Part 1 - Round ', $i, "\n";
-			echo '==========', "\n";
-		}
-		processRound($monkies);
+		if (isDebug()) { echo '==========', "\n", 'Part 1 - Round ', $i, "\n", '==========', "\n"; }
+		processRound($monkies, $worryReduction);
 	}
 	if (isDebug()) {
 		foreach ($monkies as $m) {
@@ -99,19 +72,16 @@
 		}
 	}
 
-	$part1 = array_map(fn($m) => $m['inspectCount'], $monkies);
-	rsort($part1);
-	$part1 = $part1[0] * $part1[1];
+	$part1 = array_product(array_slice(sorted('rsort', array_map(fn($m) => $m['inspectCount'], $monkies)), 0, 2));
 	echo 'Part 1: ', $part1, "\n";
 
 	$monkies = $startingMonkies;
+	$allDivisors = array_product(array_map(fn($m) => $m['test']['condition'], $monkies));
+	$worryReduction = fn($item) => $item % $allDivisors;
+
 	for ($i = 0; $i < 10000; $i++) {
-		if (isDebug() && $i < 20) {
-			echo '==========', "\n";
-			echo 'Part 2 - Round ', $i, "\n";
-			echo '==========', "\n";
-		}
-		processRound($monkies, true, (isDebug() && $i < 20));
+		if (isDebug() && $i < 20) { echo '==========', "\n", 'Part 2 - Round ', $i, "\n", '==========', "\n"; }
+		processRound($monkies, $worryReduction, (isDebug() && $i < 20));
 
 		if (isDebug() && $i == 19) {
 			echo '==========', "\n";
@@ -128,7 +98,5 @@
 		}
 	}
 
-	$part2 = array_map(fn($m) => $m['inspectCount'], $monkies);
-	rsort($part2);
-	$part2 = $part2[0] * $part2[1];
+	$part2 = array_product(array_slice(sorted('rsort', array_map(fn($m) => $m['inspectCount'], $monkies)), 0, 2));
 	echo 'Part 2: ', $part2, "\n";
