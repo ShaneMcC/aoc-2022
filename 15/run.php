25423309a933e2e3373f21f47982b2c822b22a67
Shane Mc Cormack <shanemcc@gmail.com>
2022-12-15 06:26:42 +0000

    Speed up part 1. 45s to 10s.

--- 15/run.php
+++ 15/run.php
@@ -16,8 +16,7 @@
 		$distance = manhattan($sX, $sY, $bX, $bY);
 		$sensors[$sX . ',' . $sY] = ['loc' => [$sX, $sY], 'beacon' => [$bX, $bY], 'distance' => $distance];
 
-		if (!isset($beacons[$bX . ',' . $bY])) { $beacons[$bX . ',' . $bY] = ['sensors' => []]; }
-		$beacons[$bX . ',' . $bY]['sensors'][] = [$sX, $sY];
+		$beacons[$bX . ',' . $bY] = true;
 
 		$minX = min(min($minX, $sX - $distance), $bX);
 		$maxX = max(max($maxX, $sX + $distance), $bX);
@@ -26,20 +25,28 @@
 	$y = isTest() ? 10 : 2000000;
 	$part1 = 0;
 	for ($x = $minX; $x <= $maxX; $x++) {
-		if (isset($beacons[$x . ',' . $y])) { continue; }
-		if (isset($sensors[$x . ',' . $y])) { continue; }
-
 		foreach ($sensors as $s) {
 			$distanceToClosest = $s['distance'];
 			$distanceToMe = manhattan($x, $y, $s['loc'][0], $s['loc'][1]);
 
 			if ($distanceToMe <= $distanceToClosest) {
-				$part1++;
+				// If this space is covered, others along this row
+				// will be as well, so lets bypass those.
+				$manhattanToTop = manhattan($s['loc'][0], $y, $s['loc'][0], $s['loc'][1]);
+				$newX = $s['loc'][0] + ($s['distance'] - $manhattanToTop);
+
+				$part1 += ($newX - $x) + 1;
+				$x = $newX;
 				break;
 			}
 		}
 	}
 
+	// Remove any beacons on this row.
+	foreach (array_keys($beacons) as $b) {
+		[$bX, $bY] = explode(',', $b);
+		if ($bY == $y) { $part1--; }
+	}
 	echo 'Part 1: ', $part1, "\n";
 
 	$min = 0;
@@ -54,7 +61,7 @@
 				if ($distanceToMe <= $distanceToClosest) {
 					$covered = true;
 
-					// If this space is covered, a others along this row
+					// If this space is covered, others along this row
 					// will be as well, so lets bypass those.
 					$manhattanToTop = manhattan($s['loc'][0], $y, $s['loc'][0], $s['loc'][1]);
 					$x = $s['loc'][0] + ($s['distance'] - $manhattanToTop);
