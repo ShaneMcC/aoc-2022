6707c91ff4692c8f103a7bec6c2054f7f4034891
Shane Mc Cormack <shanemcc@gmail.com>
2022-12-17 03:42:05 +0000

    Day 16. Part 2. 8919.

--- 16/run.php
+++ 16/run.php
@@ -10,86 +10,70 @@
 		$valves[$valve] = ['valve' => $valve, 'rate' => $rate, 'next' => explode(', ', $next)];
 	}
 
-    // Shortest distances between any two valves.
-    // https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm
-	function getDistances($valves) {
-		$distances = [];
-		foreach ($valves as $valve) {
-			$distances[$valve['valve']] = [$valve['valve'] => 0];
+	$distances = [];
+	foreach ($valves as $valve) {
+		$distances[$valve['valve']] = [$valve['valve'] => 0];
 
-			foreach ($valve['next'] as $next) {
-				$distances[$valve['valve']][$next] = 1;
-			}
+		foreach ($valve['next'] as $next) {
+			$distances[$valve['valve']][$next] = 1;
 		}
+	}
 
-		foreach (array_keys($valves) as $k) {
-			foreach (array_keys($valves) as $i) {
-				foreach (array_keys($valves) as $j) {
-					if (isset($distances[$i][$k]) && isset($distances[$k][$j])) {
-						if (!isset($distances[$i][$j]) || ($distances[$i][$j] > $distances[$i][$k] + $distances[$k][$j])) {
-							$distances[$i][$j] = $distances[$i][$k] + $distances[$k][$j];
-						}
+	foreach (array_keys($valves) as $k) {
+		foreach (array_keys($valves) as $i) {
+			foreach (array_keys($valves) as $j) {
+				if (isset($distances[$i][$k]) && isset($distances[$k][$j])) {
+					if (!isset($distances[$i][$j]) || ($distances[$i][$j] > $distances[$i][$k] + $distances[$k][$j])) {
+						$distances[$i][$j] = $distances[$i][$k] + $distances[$k][$j];
 					}
 				}
 			}
 		}
-
-		return $distances;
 	}
 
-	function getPressureRelease($valves, $distances) {
+	$valves = array_filter($valves, fn($a) => $a['rate'] > 0);
 
+	function getPressureRelease($valves, $distances, $maxTime = 30) {
 		$queue = new SPLQueue();
 		$states = [];
 		$visited = [];
 
-		$queue->enqueue(['open' => [], 'pressure' => 0, 'minutes' => 0, 'loc' => 'AA', 'path' => ['AA']]);
+		$queue->enqueue(['open' => [], 'flow' => 0, 'pressure' => 0, 'minutes' => 0, 'loc' => 'AA', 'path' => []]);
 
 		while (!$queue->isEmpty()) {
 			$loc = $queue->dequeue();
-
-			if ($loc['minutes'] == 30) {
-				$states[] = $loc;
-				continue;
-			} else if ($loc['minutes'] > 30) {
-				continue;
-			}
-
-			$currentPressure = 0;
-			foreach (array_keys($loc['open']) as $open) { $currentPressure += $valves[$open]['rate']; }
-
 			$options = [];
 
 			// Wait here forever:
-			if ($currentPressure > 0) {
+			if ($loc['flow'] > 0) {
 				$newLoc = $loc;
-				$diff = (30 - $newLoc['minutes']);
-				$newLoc['pressure'] += $currentPressure * $diff;
-				$newLoc['minutes'] = 30;
-				$newLoc['path'][] = 'wait';
-				// $newLoc['path'][] = '[T:' . $loc['minutes'] . ' WAIT A:' . $newLoc['minutes'] . ' P:' . $currentPressure . '*' . ($diff + 1) . ']';
-				$options[] = $newLoc;
+				$diff = ($maxTime - $newLoc['minutes']);
+				$newLoc['pressure'] += $loc['flow'] * $diff;
+				$newLoc['minutes'] = $maxTime;
+				$states[] = $newLoc;
 			}
 
 			// Move somewhere we haven't opened yet that isn't broken.
 			foreach (array_keys($valves) as $next) {
-				if (!isset($loc['open'][$next]) && $valves[$next]['rate'] > 0) {
-					$diff = $distances[$loc['loc']][$next];
+				if (!isset($loc['open'][$next])) {
+					// Time taken to move there, +1 to open it.
+					$timeTaken = $distances[$loc['loc']][$next] + 1;
 
 					$newLoc = $loc;
 					$newLoc['loc'] = $next;
 					$newLoc['open'][$next] = true;
-					$newLoc['minutes'] += $diff + 1;
-					// $newLoc['path'][] = '[T:' . $loc['minutes'] . ' M:' . $next . ' A:' . $newLoc['minutes'] . ' P:' . $currentPressure . '*' . ($diff + 1) . ']';
+					$newLoc['minutes'] += $timeTaken;
 					$newLoc['path'][] = $next;
-					$newLoc['pressure'] += $currentPressure * ($diff + 1);
-					$options[] = $newLoc;
+					$newLoc['pressure'] += $loc['flow'] * ($timeTaken);
+					$newLoc['flow'] += $valves[$next]['rate'];
+					if ($newLoc['minutes'] < $maxTime) {
+						$options[] = $newLoc;
+					}
 				}
 			}
 
 			foreach ($options as $opt) {
 				$s = implode(',', $opt['path']);
-				// echo $s, "\n";
 				if (!isset($visited[$s])) {
 					$visited[$s] = true;
 					$queue->enqueue($opt);
@@ -100,13 +84,31 @@
 		return $states;
 	}
 
-	$distances = getDistances($valves);
-	$states = getPressureRelease($valves, $distances);
-	usort($states, fn($a, $b) => $a['pressure'] <=> $b['pressure']);
-	// foreach ($states as $state) { echo implode(',', $state['path']), ' => ', $state['pressure'], "\n"; }
+	// Get all possible states for 30 minutes, return the best.
 
-	$part1 = $states[count($states) - 1]['pressure'];
+	$states30 = getPressureRelease($valves, $distances, 30);
+	usort($states30, fn($a, $b) => $a['pressure'] <=> $b['pressure']);
+	$part1 = $states30[count($states30) - 1]['pressure'];
 	echo 'Part 1: ', $part1, "\n";
 
-	// $part2 = -1;
-	// echo 'Part 2: ', $part2, "\n";
+	// Get all possible states for 26 minutes.
+	$states26 = getPressureRelease($valves, $distances, 26);
+	$part2 = 0;
+
+	// Find the best costs for each combination.
+	$bestPaths = [];
+	foreach ($states26 as $s) {
+		$nodes = implode(',', sorted('sort', $s['path']));
+		$bestPaths[$nodes] = max($bestPaths[$nodes] ?? PHP_INT_MIN, $s['pressure']);
+	}
+
+	// Now find the best pair.
+	foreach ($bestPaths as $s1 => $p1) {
+		foreach ($bestPaths as $s2 => $p2) {
+			$d = array_intersect(explode(',', $s1), explode(',', $s2));
+			if (empty($d)) {
+				$part2 = max($part2, $p1 + $p2);
+			}
+		}
+	}
+	echo 'Part 2: ', $part2, "\n";
