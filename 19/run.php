c918737588a0c1b1214f584dfa61682a1d8cf09b
Shane Mc Cormack <shanemcc@gmail.com>
2022-12-20 03:24:01 +0000

    Day 19. Part 1. 8764.

--- 19/run.php
+++ 19/run.php
@@ -6,102 +6,130 @@
 	$blueprints = [];
 	foreach ($input as $line) {
 		preg_match('#Blueprint (.*): Each ore robot costs (.*) ore. Each clay robot costs (.*) ore. Each obsidian robot costs (.*) ore and (.*) clay. Each geode robot costs (.*) ore and (.*) obsidian.#SADi', $line, $m);
+
 		[, $bp, $oreOre, $clayOre, $obsidianOre, $obsidianClay, $geodeOre, $geodeObsidian] = $m;
-		$blueprints[] = ['num' => $bp, 'costs' => ['ore' => ['ore' => $oreOre], 'clay' => ['ore' => $clayOre], 'obsidian' => ['ore' => $obsidianOre, 'clay' => $obsidianClay], 'geode' => ['ore' => $geodeOre, 'obsidian' => $geodeObsidian]], 'result' => []];
+
+		$blueprints[] = ['num' => $bp,
+		                 'costs' => ['ore' => ['ore' => $oreOre],
+		                             'clay' => ['ore' => $clayOre],
+		                             'obsidian' => ['ore' => $obsidianOre, 'clay' => $obsidianClay],
+		                             'geode' => ['ore' => $geodeOre, 'obsidian' => $geodeObsidian]
+		                            ],
+		                 'quality' => []
+		                ];
+	}
+
+	function getMaxRobots($bp) {
+		$required = [];
+		foreach ($bp['costs'] as $robotType) {
+			foreach ($robotType as $type => $cost) {
+				$required[$type] = max($required[$type] ?? 0, $cost);
+			}
+		}
+
+		$required['geode'] = PHP_INT_MAX;
+
+		return $required;
+	}
+
+	function canBuild($bp, $robotType, $materials) {
+		foreach ($bp['costs'][$robotType] as $type => $amount) {
+			if ($materials[$type] < $amount) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	function getBuildOptions($bp, $materials, $robots) {
+		$options = [];
+
+		$maxNeeded = getMaxRobots($bp);
+
+		// If we can build a robot, and we have the materials to do it.
+		// Consider it.
+		$preference = ['geode' => true, 'obsidian' => true, 'clay' => false, 'ore' => false];
+		foreach ($preference as $robotType => $onlyConsiderThis) {
+			if (canBuild($bp, $robotType, $materials) && $robots[$robotType] < $maxNeeded[$robotType]) {
+				if ($onlyConsiderThis) {
+					return [$robotType];
+				} else {
+					$options[] = $robotType;
+				}
+			}
+		}
+
+		// Build nothing, just harvest.
+		if ($materials['ore'] <= $maxNeeded['ore']) {
+			$options[] = null;
+		}
+
+		return $options;
 	}
 
 	function buildBP($bp) {
-		$result = ['quality' => -1];
+		$bestQuality = -1;
 
-		$m = 1;
 		$materials = ['ore' => 0, 'clay' => 0, 'obsidian' => 0, 'geode' => 0];
 		$robots = ['ore' => 1, 'clay' => 0, 'obsidian' => 0, 'geode' => 0];
-		$state = [$m, $materials, $robots];
+		$state = [1, $materials, $robots];
 
 		$queue = new SPLPriorityQueue();
 		$queue->setExtractFlags(SplPriorityQueue::EXTR_BOTH);
 		$queue->insert($state, 0);
 
+		$visted = [];
+
 		while (!$queue->isEmpty()) {
 			$q = $queue->extract();
-			[$m, $materials, $robots] = $q['data'];
-			$quality = $materials['geode'] * $bp['num'];
+			[$time, $materials, $robots] = $q['data'];
 
-			if ($m == 24) {
-				// Store this option if it is a better result than we had
-				// before.
-				if ($quality > $result['quality']) {
-					$result = ['materials' => $materials, 'robots' => $robots, 'quality' => $quality];
+			// Store this option if it is a better result than we had
+			// before.
+			$bestQuality = max($bestQuality, $materials['geode'] * $bp['num']);
+
+			// Don't go past minute 24.
+			if ($time > 24) { continue; }
+
+			// If we have seen this state before, don't bother doing anything
+			// again.
+			$f = json_encode($q['data']);
+			if (isset($visted[$f])) { continue; }
+			$visted[$f] = true;
+
+			foreach (getBuildOptions($bp, $materials, $robots) as $nextBuild) {
+				$nextMaterials = $materials;
+				$nextRobots = $robots;
+
+				// Harvest
+				foreach (array_keys($materials) as $type) {
+					$nextMaterials[$type] += $nextRobots[$type];
 				}
-				continue;
-			}
 
-			$options = [];
-
-			// Check what options we can build.
-			foreach (['geode', 'obsidian', 'clay', 'ore'] as $robotType) {
-				// Always build geode robots.
-				$canBuild = ($robotType == 'geode');
-
-				// If we do not benefit from building this type
-				// then do not.
-				foreach ($bp['costs'] as $c) {
-					// If this blueprint needs more of a robot type than we
-					// have, then we can consider building it.
-					if (isset($c[$robotType]) && $c[$robotType] > $robots[$robotType]) {
-						$canBuild = true;
+				if ($nextBuild != null) {
+					// Remove used up materials
+					foreach ($bp['costs'][$nextBuild] as $type => $cost) {
+						$nextMaterials[$type] -= $cost;
 					}
+
+					// Build the Robot
+					$nextRobots[$nextBuild]++;
 				}
 
-				// Check that we have enough current materials to build this
-				// robot type.
-				foreach ($bp['costs'][$robotType] as $type => $amount) {
-					if ($materials[$type] < $amount) {
-						$canBuild = false;
-					}
-				}
-
-				// If this is a robot type we want to build, then add it as
-				// a build option to our queue.
-				if ($canBuild) {
-					$o = ['build' => $robotType, 'materials' => $materials, 'robots' => $robots];
-					foreach ($bp['costs'][$robotType] as $type => $amount) {
-						$o['materials'][$type] -= $amount;
-					}
-					$options[] = $o;
-				}
-			}
-			$options[] = ['build' => null, 'materials' => $materials, 'robots' => $robots];
-
-			// Deal with the various options.
-			foreach ($options as $o) {
-				// All options harvest some material
-				foreach ($robots as $type => $amount) {
-					$o['materials'][$type] += $amount;
-				}
-
-				// Finish any applicable build.
-				if ($o['build'] != null) {
-					$o['robots'][$o['build']]++;
-				}
-
-				// Queue this option for further investigation
-				$queue->insert([($m + 1), $o['materials'], $o['robots']], -($m));
+				// Enqueue the state.
+				$queue->insert([$time + 1, $nextMaterials, $nextRobots], 0);
 			}
 		}
 
-		return $result;
+		return $bestQuality;
 	}
 
 	foreach ($blueprints as $bpid => $bp) {
-		$blueprints[$bpid]['result'] = buildBP($bp);
-		var_dump($blueprints[$bpid]['result']);
-		die();
+		$blueprints[$bpid]['quality'] = buildBP($bp);
 	}
 
-//	var_dump($blueprints);
-
-	$part1 = array_sum(array_map(fn($a) => $a['result']['quality'], $blueprints));
+	$part1 = array_sum(array_map(fn($a) => $a['quality'], $blueprints));
 	echo 'Part 1: ', $part1, "\n";
 
 	// $part2 = -1;
